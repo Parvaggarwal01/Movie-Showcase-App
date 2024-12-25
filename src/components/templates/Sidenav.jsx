17fb3517224547cbce7e927b899e519063de6ae0
Parvaggarawal01 <parvaggarwal130@gmail.com>
2024-12-25 21:34:30 +0530

    Adding API and complete making search bar fully working

--- src/components/templates/Sidenav.jsx
+++ src/components/templates/Sidenav.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import axios from "../../utils/axios";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Sidenav = () => {
+  
+
   return (
     <div className="w-[20%] h-full border-r-2 border-zinc-400 p-8 overflow-auto">
       <h1 className="text-2xl text-white font-bold ">
-        <i class="ri-tv-fill text-[#aed6dc] mr-2"></i>
+        <i className="ri-tv-fill text-[#aed6dc] mr-2"></i>
         <span className="">Spotlight</span>
       </h1>
       <nav className="flex flex-col text-zinc-400 text-xl gap-1">
@@ -13,31 +16,31 @@ const Sidenav = () => {
           New Feeds
         </h1>
         <Link className="hover:bg-[#aed6dc] hover:text-[#4a536b] rounded-lg duration-300 p-5">
-          <i class="mr-2 ri-fire-fill"></i> Trending
+          <i className="mr-2 ri-fire-fill"></i> Trending
         </Link>
         <Link className="hover:bg-[#aed6dc] hover:text-[#4a536b] rounded-lg duration-300 p-5">
-          <i class="mr-2 ri-bard-fill"></i> Popular
+          <i className="mr-2 ri-bard-fill"></i> Popular
         </Link>
         <Link className="hover:bg-[#aed6dc] hover:text-[#4a536b] rounded-lg duration-300 p-5">
-        <i class="mr-2 ri-movie-2-ai-fill"></i> Movies
+          <i className="mr-2 ri-movie-2-ai-fill"></i> Movies
         </Link>
         <Link className="hover:bg-[#aed6dc] hover:text-[#4a536b] rounded-lg duration-300 p-5">
-        <i class="mr-2 ri-tv-2-fill"></i> Tv Shows
+          <i className="mr-2 ri-tv-2-fill"></i> Tv Shows
         </Link>
         <Link className="hover:bg-[#aed6dc] hover:text-[#4a536b] rounded-lg duration-300 p-5">
-        <i class="mr-2 ri-team-fill"></i> People
+          <i className="mr-2 ri-team-fill"></i> People
         </Link>
       </nav>
-        <hr className="border-none h-[1px] bg-zinc-400" />
+      <hr className="border-none h-[1px] bg-zinc-400" />
       <nav className="flex flex-col text-zinc-400 text-xl gap-1">
         <h1 className="text-white font-semibold text-xl mt-10 mb-5">
           Website Information
         </h1>
         <Link className="hover:bg-[#aed6dc] hover:text-[#4a536b] rounded-lg duration-300 p-5">
-        <i class="mr-2 ri-information-fill"></i> About Spotlight
+          <i className="mr-2 ri-information-fill"></i> About Spotlight
         </Link>
         <Link className="hover:bg-[#aed6dc] hover:text-[#4a536b] rounded-lg duration-300 p-5">
-        <i class="mr-2 ri-phone-fill"></i>Contact
+          <i className="mr-2 ri-phone-fill"></i>Contact
         </Link>
       </nav>
     </div>
