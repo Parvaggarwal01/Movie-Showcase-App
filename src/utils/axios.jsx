05ff7328dda536b700c9e9832225b3c9815585bc
Parvaggarawal01 <parvaggarwal130@gmail.com>
2024-12-26 23:36:08 +0530

    changes

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
-  headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_TMDB_API_KEY}`,
-  },
+  // headers: {
+  //   accept: "application/json",
+  //   Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_TMDB_API_KEY}`,
+  // },
 });
 
 
