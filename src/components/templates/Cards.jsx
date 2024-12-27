a58f03eb7daa2c6c77f4ccf6bfc56ffd373c3f79
Parvaggarawal01 <parvaggarwal130@gmail.com>
2024-12-28 01:02:16 +0530

    Adding Rating Percentage in Cards

--- src/components/templates/Cards.jsx
+++ src/components/templates/Cards.jsx
@@ -6,7 +6,7 @@ const Cards = ({ data, title }) => {
   return (
     <div className="flex flex-wrap w-full h-full px-[3%] justify-between bg-[#4a536b]">
       {data.map((d, i) => (
-        <Link className="w-[26vh] mr-[5%] mb-[5%]" key={i}>
+        <Link className="relative w-[26vh] mr-[5%] mb-[5%]" key={i}>
           <img
             className="shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)] h-[35vh] object-cover"
             src={`https://image.tmdb.org/t/p/original/${
@@ -17,6 +17,11 @@ const Cards = ({ data, title }) => {
           <h1 className="text-lg text-zinc-100 mt-3 font-semibold">
             {d.name || d.title || d.original_name || d.original_title}
           </h1>
+          {d.vote_average && (
+            <div className="absolute right-[-10%] bottom-[25%] bg-yellow-600 text-lg font-semibold rounded-full text-white w-[7vh] h-[7vh] flex justify-center items-center">
+              {(d.vote_average * 10).toFixed()} <sup>%</sup>
+            </div>
+          )}
         </Link>
       ))}
     </div>
